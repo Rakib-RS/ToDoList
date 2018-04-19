77164e08dcf8a1fb11e1804414ea9692d6a23ebf
Rakib Sheikh <rakib.rscse@gmail.com>
2018-04-19 19:14:27 +0600

    delete function is added

--- delete.cpp
+++ delete.cpp
@@ -3,3 +3,19 @@ public:
     void deletetodo(int tasknumber);
 
 };
+void delete_anything::deletetodo(int tasknumber){
+    int index=1;
+    while(tasknumber<size){
+        mytodolist[tasknumber]=mytodolist[index+1];
+        tasknumber++;
+    }
+    size--;
+}
+void delete_anything::deletetodo(int tasknumber){
+    int index=1;
+    while(tasknumber<size){
+        mytodolist[tasknumber]=mytodolist[index+1];
+        tasknumber++;
+    }
+    size--;
+}
