5e5a5b142a0a764b07f84bbf360ae8007b67aad6
Rakib Sheikh <rakib.rscse@gmail.com>
2018-04-19 19:22:38 +0600

    all out function is added

--- show.cpp
+++ show.cpp
@@ -11,3 +11,17 @@ void display::show(){
         std::cout<<mytodolist[index].description<<std::endl;
     }
 }
+void display::out_choice(){
+        std::cout<<"Enter -1 to terminate the program"<<std::endl;
+        std::cout<<"Enter 1 to add a new item in the list"<<std::endl;
+        std::cout<<"Enter 2 to delete a item from the list"<<std::endl;
+        std::cout<<"Enter 3 to see the Remaining item in the list"<<std::endl;
+}
+void display::out_menu(){
+    std::cout<<"Enter taskname , description and Date\n";
+    std::cout<<"After input anything you have to press 'Enter'button to take next input\n";
+}
+
+void display::out_tasknumber(){
+    std::cout<<"Enter tasknumber that you want to delete\n";
+}
