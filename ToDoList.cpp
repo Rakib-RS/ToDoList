4201c1f58b1107032251e07f3b049eb2e2be5ddf
Rakib Sheikh <rakib.rscse@gmail.com>
2018-04-19 18:33:42 +0600

    all header file and input class is added

---
+++ ToDoList.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include "add.cpp"
+#include "delete.cpp"
+#include "show.cpp"
+int choice,tasknumber;
+std::string description,duedate,taskname;
+class input{
+public:
+    void input_choice();
+    void input_menu();
+    void input_tasknumber();
+};
